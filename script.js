152521ed63efc0091f417c0e0fc23035b64ff622
Gokup23 <976575334a@gmail.com>
2026-04-19 17:17:15 +0530

    initial phase

---
+++ script.js
@@ -0,0 +1,24 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAwuZlzcpymLKuLY62cEm5WDk7IsdYYTDM",
+
+  authDomain: "dsa-tracking.firebaseapp.com",
+
+  projectId: "dsa-tracking",
+
+  storageBucket: "dsa-tracking.firebasestorage.app",
+
+  messagingSenderId: "268678274250",
+
+  appId: "1:268678274250:web:0a47bce2db68791cc92833",
+
+  measurementId: "G-X7NX1BMMPX"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+console.log("Firebase connected successfully ✅");
